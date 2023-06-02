ff935fe93d506bf3213a84f87cc122c6b573c350
unknown <lspardo@uniboyaca.edu.com>
2023-06-01 20:13:12 -0500

    primer commit

---
+++ database/migrations/2023_06_02_011116_create_actividades.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('actividades', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            $table->decimal('nota');
+            $table->unsignedBigInteger('id_estudiante');
+            $table->foreign('id_estudiante')->references('id')->on('estudiante');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('actividades');
+    }
+};
